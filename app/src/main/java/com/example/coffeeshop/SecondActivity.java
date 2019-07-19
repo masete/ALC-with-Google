389ef14b846e86448a5935711e5b346f8fdc2d38
masete <nicholasmasete72@gmail.com>
2019-07-19 11:35:51 +0300

    second activity

--- app/src/main/java/com/example/coffeeshop/SecondActivity.java
+++ app/src/main/java/com/example/coffeeshop/SecondActivity.java
@@ -1,4 +1,28 @@
 package com.example.coffeeshop;
 
-public class SecondActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+//        View.v = findViewById(R.id.button2);
+//        v.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View arg0){
+        if(arg0.getId()== R.id.button2){
+            Intent intent = new Intent(this, SecondActivity.class);
+
+            this.startActivity(intent);
+        }
+    }
 }
+
